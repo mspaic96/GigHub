f6f09be3ed4fe7af7146953168ec93b576ed26d3
Milutin Spaic <milutin.spaic@bankom.rs>
2021-03-01 23:17:44 +0100

    Create Add a Gig Form!

---
+++ GigHubMosh/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GigHubMosh.Models;
+using System.Collections.Generic;
+
+namespace GigHubMosh.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date  { get; set; }
+
+        public string Time  { get; set; }
+
+        public int Genre { get; set; }
+
+
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
