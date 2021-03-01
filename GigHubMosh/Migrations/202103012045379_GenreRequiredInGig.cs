c9b5ff4fd3e2df21656f7f1c7fbc65e15121e1eb
Milutin Spaic <milutin.spaic@bankom.rs>
2021-03-01 22:01:21 +0100

    Gigs and Genres Migrations!

---
+++ GigHubMosh/Migrations/202103012045379_GenreRequiredInGig.cs
@@ -0,0 +1,26 @@
+namespace GigHubMosh.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class GenreRequiredInGig : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Gigs", "Genre_Id", "dbo.Genres");
+            DropIndex("dbo.Gigs", new[] { "Genre_Id" });
+            AlterColumn("dbo.Gigs", "Genre_Id", c => c.Byte(nullable: false));
+            CreateIndex("dbo.Gigs", "Genre_Id");
+            AddForeignKey("dbo.Gigs", "Genre_Id", "dbo.Genres", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Gigs", "Genre_Id", "dbo.Genres");
+            DropIndex("dbo.Gigs", new[] { "Genre_Id" });
+            AlterColumn("dbo.Gigs", "Genre_Id", c => c.Byte());
+            CreateIndex("dbo.Gigs", "Genre_Id");
+            AddForeignKey("dbo.Gigs", "Genre_Id", "dbo.Genres", "Id");
+        }
+    }
+}
