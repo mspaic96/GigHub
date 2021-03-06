69f03d9c8eac561f456d1dab98e62f696c9fe776
Milutin Spaic <milutin.spaic@bankom.rs>
2021-03-06 23:43:12 +0100

    Add a name to Artist!

--- GigHubMosh/Controllers/HomeController.cs
+++ GigHubMosh/Controllers/HomeController.cs
@@ -1,16 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GigHubMosh.Models;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GigHubMosh.Controllers
 {
+
+
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcomingsGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Where(g => g.DateTime > DateTime.Now);
+            return View(upcomingsGigs);
         }
 
         public ActionResult About()
--- GigHubMosh/Models/AccountViewModels.cs
+++ GigHubMosh/Models/AccountViewModels.cs
@@ -64,6 +64,10 @@ namespace GigHubMosh.Models
 
     public class RegisterViewModel
     {
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
