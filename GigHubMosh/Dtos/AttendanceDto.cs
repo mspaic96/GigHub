c69e56a38463ebf931f96e081682839006aa8008
Milutin Spaic <milutin.spaic@bankom.rs>
2021-03-14 19:09:56 +0100

    Implementing attendances !

---
+++ GigHubMosh/Dtos/AttendanceDto.cs
@@ -0,0 +1,7 @@
+﻿namespace GigHubMosh.Dtos
+{
+    public class AttendanceDto
+    {
+        public int GigId { get; set; }
+    }
+}
--- GigHubMosh/Models/IdentityModels.cs
+++ GigHubMosh/Models/IdentityModels.cs
@@ -28,6 +28,8 @@ namespace GigHubMosh.Models
         public DbSet<Gig> Gigs { get; set; }
 
         public DbSet<Genre> Genres  { get; set;}
+
+        public DbSet<Attendance> Attendances { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -36,6 +38,14 @@ namespace GigHubMosh.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {   modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
